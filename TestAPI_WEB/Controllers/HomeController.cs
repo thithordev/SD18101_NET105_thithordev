3317dae230dad7a88fb21d90595b1d595738c37e
shanghaik168@gmail.com <Shanghaik1998>
2023-09-18 09:01:29 +0700

    API CALL _2

---
+++ TestAPI_WEB/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using TestAPI_WEB.Models;
+
+namespace TestAPI_WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Privacy(double money, double percent, int month)
+        {
+            string requestURL = $"https://localhost:7184/WeatherForecast/" +
+    $"get-double-profit?a={money}&b={percent}&c={month}";
+            var HttpClient = new HttpClient(); // Tạo ra 1 httpClient 
+            var response = HttpClient.GetAsync(requestURL).Result; // Đọc responseBody từ URL
+            string apiData = await response.Content.ReadAsStringAsync();
+            // Lấy dữ liệu lấy được chuyển sang dạng string
+            // Lấy kết quả vừa thu được ép kiểu sang kiểu dữ liệu mình cần
+            var result = JsonConvert.DeserializeObject<int>(apiData);
+            ViewData["result"] = result;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
